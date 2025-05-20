1a22a7c7678443224ce72e57f1ddebedbcea1050
beshar jeiroudi <lastking711@protonmail.com>
2025-05-20 10:02:45 -0700

    'a'

--- database/migrations/2025_05_19_160543_project_role_user.php
+++ database/migrations/2025_05_19_160543_project_role_user.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained();
             $table->foreignId('role_id')->constrained();
             $table->foreignId('project_id')->constrained();
+            $table->timestamps();
         });
     }
 
